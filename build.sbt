0bd3a2f53623939b07fc2f3b976e515b88e8da6e
tmcgahey93 <tmcgahey93@gmail.com>
2025-06-08 20:15:53 -0400

    New Spark App

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "TaxiQuery"
+version := "0.1"
+scalaVersion := "2.12.18"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.1"
---
+++ src/main/scala/TaxiQuery.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.sql.SparkSession
+
+object TaxiQuery {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("NYC Taxi Trip Analysis")
+      .master("local[*]")
+      .getOrCreate()
+
+    val df = spark.read.parquet("yellow_tripdata_2024-12.parquet")
+    df.createOrReplaceTempView("taxi")
+
+    val result = spark.sql("""
+      SELECT passenger_count, COUNT(*) as trip_count
+      FROM taxi
+      WHERE passenger_count > 0
+      GROUP BY passenger_count
+      ORDER BY trip_count DESC
+    """)
+
+    result.show()
+
+    spark.stop()
+  }
+}
